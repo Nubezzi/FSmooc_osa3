e5371b1591b3809bb24677e5e2a634e13468e3ff
Nubezzi <patenur@hotmail.com>
2022-07-13 00:24:00 +0300

    putsaamista ja 3.19

--- index.js
+++ index.js
@@ -15,30 +15,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :J
 app.use(express.static('build'))
 
 
-/*let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-    
-  ] */
-
 app.get('/info', (req, res) => {
   let sum = 0
   Person.find().count(function (err, count) {
@@ -85,10 +61,6 @@ app.get('/api/persons/:id', (request, response, next) => {
       next(error)
     })
 })
-
-/*const generateId = () => {
-    return Math.floor(Math.random() * randMax)
-  } */
 
   app.put('/api/persons/:id', (request, response, next) => {
     const { name, number } = request.body
