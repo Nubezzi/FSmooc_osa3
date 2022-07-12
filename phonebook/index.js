bfe504830d5c96b4ae73d0283edd2ef0a016affc
Nubezzi <patenur@hotmail.com>
2022-07-12 17:06:30 +0300

    init

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
 const app = express()
 const randMax = 100000
 morgan.token('JSONcontent', function (req, res) { return JSON.stringify(req.body) })
@@ -93,7 +96,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
-const PORT = 3001
+  const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
